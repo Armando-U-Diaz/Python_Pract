6b0f0643520f06113e88e63af7322eb240471c3c
Diaz ArmandoU <diaz.armando.u@gmail.com>
2024-11-29 21:21:52 -0600

    Create Variables_Python.py
    Como se deben usar las variables al crear o usar las variables dentro del código de Python

---
+++ Variables_Python.py
@@ -0,0 +1,24 @@
+var = "Hola mundo"
+print(var)
+var = "Adios mundo"
+print(var)
+#asignacion multiple
+var = "Hola mundo de nuevo"
+var2 = var
+print(var2)
+#sintaxis de las variables en python
+##variabl con caracter de subrayado
+_var = "Hola papu"
+print(_var)
+##variable con mayusculas y minusculas
+vAr = "Hola amigos"
+print(vAr)
+##variable con letras, linea de subrayado y numero
+var_2 = "Hola chavos"
+print(var_2)
+## no se pueden utilizar los caracteres como: &
+##las variables nunca empiezan con un numero
+##El nombre de las variables son sencibles con mayusculas y minusculas
+##eso quiere decir que se debe escribir la variable igual en
+##t odo lo que se ocupe
+
