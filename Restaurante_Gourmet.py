08ce44bff6fb50452948ab42f4172f2d61e46980
Diaz ArmandoU <diaz.armando.u@gmail.com>
2024-11-29 21:48:12 -0600

    Create Restaurante_Gourmet.py
    primera practica en python utilizando variables e imprimiéndolas

---
+++ Restaurante_Gourmet.py
@@ -0,0 +1,19 @@
+nombre_entrante = "Sopa de trufas"
+precio_entrante = "12 euros"
+nombre_principal = "Filete Mignon con salsa de vino tinto"
+precio_principal = "30 euros"
+nombre_postre = "Tiramisú casero"
+precio_postre = "8 euros"
+print("Menú del Día - Restaurante Gourmet")
+print("--Entrante--")
+print(nombre_entrante)
+print("Precio:")
+print(precio_entrante)
+print("--Plato Principal--")
+print(nombre_principal)
+print("Precio:")
+print(precio_principal)
+print("--Postre--")
+print(nombre_postre)
+print("Precio:")
+print(precio_postre)
